d2dae1aba0b3f78b7bddf8d26374268008a2a597
Jeremy Weaver <weavejer26@gmail.com>
2025-11-06 12:57:59 -0700

    Added Shopping Cart test

--- test/pageobjects/secure.page.js
+++ test/pageobjects/secure.page.js
@@ -12,8 +12,49 @@ class SecurePage extends Page {
      */
     get productsContainer() {
         return $('#inventory_container');
+   };
+
+//Shopping Cart, This test will use the Sauce Labs Backpack
+
+   get cartBadge() {
+    return $('//span[@class="shopping_cart_badge"]');
    }
 
+   get addToCartButton () { 
+    return $('button[data-test^="add-to-cart-sauce-labs-backpack"]');
+   }
+
+   get cartIcon() {
+    return $('a.shopping_cart_link');
+   }
+
+   get removeButton() {
+    return $('button[data-test^="remove-sauce-labs-backpack"]');
+   }
+
+   get cartitemContainer() {
+    return $('#item_4_title_link');
+   }
+
+   async addProductBackpack() {
+        await this.addToCartButton.click();
+        
+        await expect(this.cartBadge).toHaveText('1');
+    }
+    
+    async viewCart() {
+        await this.cartIcon.click();
+        
+        await expect(browser).toHaveUrl('https://www.saucedemo.com/cart.html');
+    }
+
+    async removeProductBackpack() {
+        await this.removeButton.click();
+        await expect(this.cartitemContainer).not.toBeDisplayed();
+        await expect(this.cartBadge).not.toBeDisplayed();
+    }
+    
+   
 //BurgerMenu
     get allItems() {
         return $('.bm-item-list a');
@@ -27,7 +68,7 @@ class SecurePage extends Page {
 
     async burgerMenuVisible() {
         await this.openBurgerMenu();
-
+        
         await expect(this.aboutLink).toBeDisplayed();
     
     return this;
--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -2,8 +2,7 @@ import { expect } from '@wdio/globals'
 import LoginPage from '../pageobjects/login.page.js'
 import SecurePage from '../pageobjects/secure.page.js'
 
-//LOGIN
-
+//Login and Burger Menu Test
 //standard_user
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
@@ -26,8 +25,7 @@ describe('My Login application', () => {
     });
 
 //About URL
-
-    it('Should have About URL work without navigating to it', async () => {
+    it('Should check that the "About" URL works without navigating to it', async () => {
         await LoginPage.open();
         await LoginPage.login('standard_user', 'secret_sauce');
 
@@ -35,6 +33,7 @@ describe('My Login application', () => {
 
         await SecurePage.aboutLinkHref();
     }); 
+
 //Logout
     it('Should successfully logout from burger menu', async () => {
         await LoginPage.open();
@@ -43,4 +42,19 @@ describe('My Login application', () => {
         await SecurePage.logout();
     })
     
+});
+
+//Shopping Cart Test
+describe('Shopping Cart Test', () => {
+    beforeEach(async () => {
+        await LoginPage.open();
+        await LoginPage.login('standard_user', 'secret_sauce');
+    });
+
+    it('Should successfully add backpack, go to cart page, and then remove backpack', async () => {
+        await SecurePage.addProductBackpack();
+        await SecurePage.viewCart();
+
+        await SecurePage.removeProductBackpack();
+    });
 });
